0a8d655e3d9e3bba82219267fb02335650ff558e
Bethel <28558632+bettymelesse@users.noreply.github.com>
2022-12-26 09:24:17 -0800

    added molsider

--- lib/models/molsider/model.py
+++ lib/models/molsider/model.py
@@ -2,4 +2,4 @@ from ..egt_molgraph import EGT_MOL
 
 class EGT_MOLSIDER(EGT_MOL):
     def __init__(self, **kwargs):
-        super().__init__(output_dim=1, **kwargs)
+        super().__init__(output_dim=27, **kwargs)
