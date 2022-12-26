81873a9dea49e75c4e9503d0b9bcc624cf3ac95b
Bethel <28558632+bettymelesse@users.noreply.github.com>
2022-12-27 00:18:16 +0900

    Added molhiv configs

--- run_training.py
+++ run_training.py
@@ -2,5 +2,7 @@ import sys
 from lib.training.execute import get_configs_from_args, execute
 
 if __name__ == '__main__':
+    # args = sys.argv
+    # args.append('configs/molhiv/egt_110m.yaml')
     config = get_configs_from_args(sys.argv)
     execute('train', config)
